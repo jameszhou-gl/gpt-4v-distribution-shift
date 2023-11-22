69fcb03deb54f06031039bf60c4408a899ae5cca
jameszhou_gl <jameszhou.ustc@gmail.com>
2023-11-22 19:50:33 +1100

    accept only one dataset as input in clip, modify output_dir in eval_clip

--- evaluation/eval_clip.py
+++ evaluation/eval_clip.py
@@ -12,8 +12,9 @@ from data.random_sampler import gen_sample_json
 
 def setup(args):
     # setup output directory and logging
-    current_time = datetime.now().strftime('%Y-%m-%d-%H_%M_%S')
-    args.output_dir = f"{args.output_dir}/{current_time}"
+    if args.output_dir[-6:] in 'exp_output':
+        current_time = datetime.now().strftime('%Y-%m-%d-%H_%M_%S')
+        args.output_dir = f"{args.output_dir}/{current_time}"
     os.makedirs(args.output_dir)
     logger = setup_logging(args.output_dir)
     logger.info(args)
@@ -26,43 +27,42 @@ def main(args):
     processor = CLIPProcessor.from_pretrained(f'openai/{args.model_name}')
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model.to(device)
-    for each_dataset in args.dataset:
-        gen_sample_json(dataset=each_dataset, args=args)
-        # Load the JSON file
-        with open(f'{args.output_dir}/unified_input_{each_dataset}.json', 'r') as f:
-            data = json.load(f)
-        # Extract class names from the JSON file
-        class_names = data['class_names']
+    gen_sample_json(dataset=args.dataset, args=args)
+    # Load the JSON file
+    with open(f'{args.output_dir}/unified_input_{args.dataset}.json', 'r') as f:
+        data = json.load(f)
+    # Extract class names from the JSON file
+    class_names = data['class_names']
 
-        for item_id, item in tqdm(data['samples'].items(), desc="Processing Samples"):
-            unified_output = {}
-            image_path = os.path.join(
-                args.data_dir, each_dataset, item['image'])
-            # Load the image
-            image = Image.open(image_path).convert("RGB")
-            # Process the image and text with CLIP processor
-            inputs = processor(text=class_names, images=image,
-                               return_tensors="pt", padding=True)
-            inputs = {k: v.to(device) for k, v in inputs.items()}
+    for item_id, item in tqdm(data['samples'].items(), desc="Processing Samples"):
+        unified_output = {}
+        image_path = os.path.join(
+            args.data_dir, args.dataset, item['image'])
+        # Load the image
+        image = Image.open(image_path).convert("RGB")
+        # Process the image and text with CLIP processor
+        inputs = processor(text=class_names, images=image,
+                           return_tensors="pt", padding=True)
+        inputs = {k: v.to(device) for k, v in inputs.items()}
 
-            # Process the image with CLIP and get the predicted class label
-            with torch.no_grad():
-                outputs = model(**inputs)
-                logits_per_image = outputs.logits_per_image
-                probs = logits_per_image.softmax(dim=1)
-                predicted_class_id = probs.argmax().item()
-                predicted_class = class_names[predicted_class_id]
+        # Process the image with CLIP and get the predicted class label
+        with torch.no_grad():
+            outputs = model(**inputs)
+            logits_per_image = outputs.logits_per_image
+            probs = logits_per_image.softmax(dim=1)
+            predicted_class_id = probs.argmax().item()
+            predicted_class = class_names[predicted_class_id]
 
-            # Store unified output jsonl
-            unified_output['dataset'] = each_dataset
-            unified_output['domain'] = item['domain']
-            unified_output['subject'] = item['subject']
-            unified_output['true_class'] = item['class']
-            unified_output['predicted_class'] = predicted_class
-            unified_output['image'] = item['image']
-            unified_output['id'] = item_id
-            with open(f'{args.output_dir}/unified_output_{args.model_name}.jsonl', 'a') as jsonl_file:
-                jsonl_file.write(json.dumps(unified_output) + '\n')
+        # Store unified output jsonl
+        unified_output['dataset'] = args.dataset
+        unified_output['domain'] = item['domain']
+        unified_output['subject'] = item['subject']
+        unified_output['true_class'] = item['class']
+        unified_output['predicted_class'] = predicted_class
+        unified_output['image'] = item['image']
+        unified_output['id'] = item_id
+        with open(f'{args.output_dir}/unified_output_{args.model_name}.jsonl', 'a') as jsonl_file:
+            jsonl_file.write(json.dumps(unified_output) + '\n')
 
     logger.info("CLIPModel processes completed.")
     analyse_unified_output(args)
@@ -73,7 +73,7 @@ if __name__ == '__main__':
         description='Evaluate CLIP in distribution shifts')
     parser.add_argument('--data_dir', type=str,
                         default="/home/guanglinzhou/scratch/gpt-4v-distribution-shift")
-    parser.add_argument('--dataset', type=str, nargs='+', default=["PACS"])
+    parser.add_argument('--dataset', type=str, default="PACS")
     parser.add_argument('--output_dir', type=str, default="./exp_output")
     parser.add_argument('--num_sample', type=int, default=20,
                         help="the number of samples for each class")
