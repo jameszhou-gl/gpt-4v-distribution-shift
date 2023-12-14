cce21515a440fa067278a239c6c50693059e3baf
jameszhou-gl <jameszhou.ustc@gmail.com>
2023-12-14 14:25:36 +1100

    Update gpt-4v_eval_pipeline.sh

--- evaluation/gpt-4v_eval_pipeline.sh
+++ evaluation/gpt-4v_eval_pipeline.sh
@@ -7,7 +7,7 @@
 CONTINUE_DIR="exp_output/2023-11-22-19_18_50"
 # Number of random and failure cases to prepare for GPT-4V evaluation
 NUM_RAND=1800 # 1800 in default
-NUM_FAILURE=10 # 180 in default
+NUM_FAILURE=180 # 180 in default
 
 # Copy the bash script to the new output directory
 cp evaluation/gpt-4v_eval_pipeline.sh "$CONTINUE_DIR"
@@ -16,14 +16,14 @@ cp evaluation/gpt-4v_eval_pipeline.sh "$CONTINUE_DIR"
 echo "Preparing GPT-4V evaluation dataset..."
 python evaluation/prepare_gpt4v_evaluation.py --num_rand $NUM_RAND --num_failure $NUM_FAILURE --continue_dir $CONTINUE_DIR
 
-# # Run GPT-4V evaluation for different scenarios
-# # Scenario 1: Failure cases, Part 1
-# echo "Running GPT-4V evaluation for Failure Scenario 1..."
-# python evaluation/gpt-4v_scenario_runner.py --continue_dir $CONTINUE_DIR --scenario_name failure_1 --openai_api_key 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
+# Run GPT-4V evaluation for different scenarios
+# Scenario 1: Failure cases, Part 1
+echo "Running GPT-4V evaluation for Failure Scenario 1..."
+python evaluation/gpt-4v_scenario_runner.py --continue_dir $CONTINUE_DIR --scenario_name failure_1 --openai_api_key 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
 
-# # Scenario 2: Failure cases, Part 2
-# echo "Running GPT-4V evaluation for Failure Scenario 2..."
-# python evaluation/gpt-4v_scenario_runner.py --continue_dir $CONTINUE_DIR --scenario_name failure_2 --openai_api_key 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
+# Scenario 2: Failure cases, Part 2
+echo "Running GPT-4V evaluation for Failure Scenario 2..."
+python evaluation/gpt-4v_scenario_runner.py --continue_dir $CONTINUE_DIR --scenario_name failure_2 --openai_api_key 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
 
 # Scenario 1: Random cases, Part 1
 echo "Running GPT-4V evaluation for Random Scenario 1..."
